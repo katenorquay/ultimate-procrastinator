127a52bd565b3798c1d1939ca8f2808de171185a
katenorquay <katenorquay@live.com>
2017-02-22 14:05:30 +1300

    hacker news api call working

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+var dotenv = require('dotenv')
+dotenv.load()
 var express = require('express')
 var expresshbs = require('express-handlebars')
 var path = require('path')
 var bodyParser = require('body-parser')
 var request = require('superagent')
 var tumblr = require('tumblr.js')
+
+require('dotenv').config()
+
 
 var app = express()
 
@@ -21,9 +26,9 @@ app.get('/', function(req, res) {
 
 app.post('/feed', function(req, res) {
   // redditAPI()
-  tumblrAPI()
+  // mediumAPI()
+  hackerNewsAPI()
   // twitterAPI()
-  // someOtherAPI()
   // anotherAPI()
   res.render('Feed')
 })
@@ -32,6 +37,7 @@ function redditAPI() {
   request
     .get("https://www.reddit.com/r/tech/top/.json?count=20")
     .end(function (err, res) {
+      if (err) {console.log(err)}
       for (var i = 0; i < res.body.data.children.length; i++) {
         if (i >= 0) {
           var Redditobj = {
@@ -46,14 +52,26 @@ function redditAPI() {
     });
 }
 
-function tumblrAPI() {
+
+function hackerNewsAPI() {
   request
-    .get('http://api.tumblr.com/v2/tagged?tag=gif')
-    .set('api_key',)
+    .get("https://community-hacker-news-v1.p.mashape.com/topstories.json?print=pretty")
+    .set("X-Mashape-Key", process.env.HACKER_NEWS_API_KEY)
+    .set("Accept", "application/json")
     .end(function (err, res) {
-      console.log(res)
-    })
+      if (err) { console.log(err)}
+      else {
+        var topStories = res.body.slice(0, 5)
+        getStories(topStories)
+      }
+    });
 }
+
+function getStories(topStories) {
+  console.log(topStories)
+}
+
+
 
 
 
